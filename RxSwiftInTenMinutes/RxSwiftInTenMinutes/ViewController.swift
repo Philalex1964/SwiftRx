5bd9c35f5d3a6c5fbae42c1bf864d0e28802121a
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-19 16:37:05 +0200

    Observable Sequences

--- RxSwiftInTenMinutes/RxSwiftInTenMinutes/ViewController.swift
+++ RxSwiftInTenMinutes/RxSwiftInTenMinutes/ViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    let bag = DisposeBag()
+    
+    let helloSequence = Observable.just("Hello Rx")
+    let fibonacciSequence = Observable.from([0,1,1,2,3,5,8])
+    let dictSequence = Observable.from([1:"Hello",2:"World"])
+    let worldSequence = Observable.from(["W", "o", "r", "l", "d"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var subscription = helloSequence.subscribe { event in
+          print(event)
+        }
+        
+        subscription = fibonacciSequence.subscribe { event in
+            print(event)
+        }
+        
+        subscription = dictSequence.subscribe { event in
+            print(event)
+        }
+        
+        subscription = worldSequence.subscribe { event in
+            switch event {
+                case .next(let value):
+                    print(value)
+            case .error(let error):
+                print(error)
+            case .completed:
+                print("completed")
+            }
+        }
+        
+        subscription.disposed(by: bag)
     }
 
 
