c5338ea6517fa67f2581d316fcb0c8801e870fd0
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-20 09:22:21 +0200

    PublishSubject

--- RxSwiftInTenMinutes/RxSwiftInTenMinutes/ViewController.swift
+++ RxSwiftInTenMinutes/RxSwiftInTenMinutes/ViewController.swift
@@ -11,38 +11,56 @@ import RxSwift
 class ViewController: UIViewController {
     let bag = DisposeBag()
     
-    let helloSequence = Observable.just("Hello Rx")
-    let fibonacciSequence = Observable.from([0,1,1,2,3,5,8])
-    let dictSequence = Observable.from([1:"Hello",2:"World"])
-    let worldSequence = Observable.from(["W", "o", "r", "l", "d"])
-    
+    var publishSubject = PublishSubject<Any>()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var subscription = helloSequence.subscribe { event in
-          print(event)
+        let subscription1 = publishSubject.subscribe(onNext:{
+          print("s1 \($0)")
+        }).disposed(by: bag)
+        // Subscription1 receives these 2 events, Subscription2 won't
+        
+        publishSubject.onNext("Hello")
+        publishSubject.onNext("World")
+
+        // Sub2 will not get "Hello" and "Again" because it susbcribed later
+        let subscription2 = publishSubject.subscribe(onNext:{
+          print(#line,$0)
+        }).disposed(by: bag)
+        
+        publishSubject.onNext("Both Subscriptions receive this message")
+        
+        let firstSubscription = publishSubject.subscribe {
+            print("first: \($0)")
         }
         
-        subscription = fibonacciSequence.subscribe { event in
-            print(event)
+        publishSubject.onNext("1")
+        publishSubject.onNext("2")
+        
+        let secondSubscription = publishSubject.subscribe {
+            print("second: \($0)")
         }
         
-        subscription = dictSequence.subscribe { event in
-            print(event)
+        publishSubject.onNext("3")
+        publishSubject.onNext("4")
+        
+        let thirdSubscription = publishSubject.subscribe {
+            print("second: \($0)")
         }
         
-        subscription = worldSequence.subscribe { event in
-            switch event {
-                case .next(let value):
-                    print(value)
-            case .error(let error):
-                print(error)
-            case .completed:
-                print("completed")
-            }
-        }
+        firstSubscription.dispose()
         
-        subscription.disposed(by: bag)
+        publishSubject.onNext("5")
+        
+        secondSubscription.dispose()
+        thirdSubscription.dispose()
+        
+        publishSubject.onNext("0")
+        
+        publishSubject.onCompleted()
+        
+        publishSubject.onNext("10")
     }
 
 
